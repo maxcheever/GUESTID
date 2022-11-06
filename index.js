a77dc1bd8324994b03e49ef660611eb42f8c7b6a
John Black <johnblack7497@gmail.com>
2022-11-06 10:25:45 -0500

    js ajax to run python, css fonts

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 document.getElementById("submitButton").addEventListener("click", outputOfText);
 
 function outputOfText(){
+    //Set the output text to the input text
     document.getElementById("outputText").innerText = document.getElementById("text-input").value
-}
+
+    //Run analyze.py to start HTML scraper
+    $.ajax({
+        url: "analyze.py",
+        context: document.body});
+    }

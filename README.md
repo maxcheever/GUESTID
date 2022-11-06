e7c48f068239d56d9dee0d579f6183b16f6e2cb9
John Black <johnblack7497@gmail.com>
2022-11-06 12:26:15 -0500

    Format output text with placeholder for hardcode

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # CONVERSATE
 Conversate is a chrome extension created with the purpose of helping all understand regional conversational dialects in the English language.
 So far, the functionality of this extension allows for efficient gathering of information to save time while researching and improve access to information.
+
+Devpost: https://devpost.com/software/conversate
--- index.js
+++ index.js
@@ -1,8 +1,12 @@
+//Create event listener for "Submit" button
 document.getElementById("submitButton").addEventListener("click", outputOfText);
 
 function outputOfText(){
     //Set the output text to the input text
-    document.getElementById("outputText").innerText = document.getElementById("text-input").value
+    //document.getElementById("outputText").innerText = document.getElementById("text-input").value
+
+    //Set the output to a hard-coded input for showcase demonstration
+    document.getElementById("outputText").innerText = "Output text goes here"
 
     //Run analyze.py to start HTML scraper
     $.ajax({
